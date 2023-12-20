dd448502321c4988fa99d1707a94703bd2e4d746
Aneesh Ajay Akut <135816561+aneeshakut@users.noreply.github.com>
2023-12-20 15:44:23 +0530

    Add files via upload

---
+++ WeatherApp.js
@@ -0,0 +1,28 @@
+let weather={
+    "apiKey":"7a02e2f64891fea3d2276a3ef693d516",
+    fetchWeather:function(city){
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=7a02e2f64891fea3d2276a3ef693d516")
+        .then((response)=>response.json())
+        .then((data)=>this.displayWeather(data))
+    },
+    displayWeather:function(data){
+        const{name}=data;
+        const {icon,description}=data.weather[0];
+        const {temp,humidity}=data.main;
+        const {speed}=data.wind;
+        console.log(name,icon,description,temp,humidity,speed);
+        document.querySelector(".city").innerHTML="Weather in " + name;
+        document.querySelector(".icon").src= "https://openweathermap.org/img/wn/" + icon +".png";
+        document.querySelector(".description").innerHTML=description;
+        document.querySelector(".temp").innerHTML=temp+"°C";
+        document.querySelector(".humidity").innerHTML="Humidity:" + humidity + "%";
+        document.querySelector(".wind").innerHTML="Wind Speed:" + speed + "km/hr";
+    },
+    search:function(){
+        this.fetchWeather(document.querySelector(".searchbar").value);
+    }
+};
+
+document.querySelector(".search button").addEventListener("click",function(){
+    weather.search();
+});
